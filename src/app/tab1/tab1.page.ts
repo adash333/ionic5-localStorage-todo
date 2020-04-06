c8269fe84e42a44bae13375cb45b045649a89d03
adash333 <adash333@hotmail.com>
2020-04-06 23:00:15 +0900

    削除の実装

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  title = 'タスク一覧';
+  todos: { name: string }[] = [
+    { name: 'タスク1' },
+    { name: 'タスク2' }
+  ];
+  // todo: string;
 
-  constructor() {}
+  constructor(
+    public actionSheetController: ActionSheetController
+  ) {}
+
+  ionViewWillEnter() {
+    if ('todos' in localStorage) {
+      this.todos = JSON.parse(localStorage.todos);
+    }
+  }
+
+  async changeTodo(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Destructive clicked');
+            this.todos.splice(index, 1);
+            localStorage.tasks = JSON.stringify(this.todos);
+          }
+        }, {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            console.log('Active clicked');
+          }
+        }, {
+          text: '閉じる',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    await actionSheet.present();
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  title = 'TODO登録';
+  todos: { name: string }[];
+  todo: string;
   constructor() {}
+
+  ionViewWillEnter() {
+    if ('todos' in localStorage) {
+      this.todos = JSON.parse(localStorage.todos);
+    }
+  }
+
+  addTodo() {
+    // todos配列の最後に、this.taskを追加する
+    this.todos.push({
+      name: this.todo
+    });
+    // localStorageのtodosという場所に、this.todosを文字列に変換したものを保存する
+    localStorage.todos = JSON.stringify(this.todos);
+    // this.todoを空文字にする
+    this.todo = '';
+  }
 
 }
